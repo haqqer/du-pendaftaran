817511bb7976ba1a889531e59f9fcbbb7a1b0505
haqqer <ilhamserbaguna@gmail.com>
2019-01-08 15:10:25 +0700

    fix path

--- src/routes/daftar.route.js
+++ src/routes/daftar.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const path = require('path');
 
 const daftarController = require('../controllers/daftar.controller');
 
