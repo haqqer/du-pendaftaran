b6f1e481944bd1b7b322defe627895d2e690ee57
haqqer <ilhamserbaguna@gmail.com>
2019-01-08 15:52:43 +0700

    add upload static

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ if(process.env.NODE_ENV !== 'production') {
 }
 const app = express();
 app.use(cors());
-app.use('/uploads', express.static(__dirname +'/uploads'));
+app.use('/uploads', express.static('uploads'));
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
