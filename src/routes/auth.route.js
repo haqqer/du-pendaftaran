54601a21be6dfdce57db06b4ab4c75fb6bd2e0f6
haqqer <ilhamserbaguna@gmail.com>
2019-01-15 15:19:50 +0700

    update login feature

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/login', (req, res, next) => {
-    res.send(req.flash('error'))
-})
-router.post('/login', passport.authenticate('local', {failureRedirect: '/login', failureFlash: true} ));
+// router.get('/login', (req, res, next) => {
+//     res.send(req.flash('error'))
+// })
+router.post('/login', passport.authenticate('local'),  (req, res) => {
+    res.json({message: "Your Login Now"})
+});
 
 router.get('/logout', (req, res) => {
     req.logout();
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,8 +5,8 @@ const userController = require('../controllers/user.controller');
 
 
 router.get('/', userController.index);
-router.post('/', auth.checkAuth, userController.store);
-router.delete('/:id', auth.checkAuth, userController.delete);
+router.post('/', userController.store);
+router.delete('/:id', userController.delete);
 // router.put('/:id', userController.update);
 // router.get('/:id', userController.show);
 
