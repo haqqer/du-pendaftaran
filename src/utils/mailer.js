7968edefe5ef76a3f3268b00513705eafc78d77e
haqqer <ilhamserbaguna@gmail.com>
2019-01-16 22:39:11 +0700

    some fixing

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail');
 
 const Mailer = (to, html) => {
     console.log('trigger')
-    sgMail.setApiKey('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');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: to,
       from: 'ilhamserbaguna@gmail.com',
